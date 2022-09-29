694a6071def2f29b5f7089f261809fbfc5bcb7c1
Loveneesh <l.ksingla2@gmail.com>
2022-09-29 16:18:55 +0530

    fixed

--- src/Services/Setup.js
+++ src/Services/Setup.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AXIOS = axios.create({
-  baseURL: "https://209.97.142.219:8080",
+  baseURL: "http://209.97.142.219:8080",
 });
 
 AXIOS.interceptors.request.use(
