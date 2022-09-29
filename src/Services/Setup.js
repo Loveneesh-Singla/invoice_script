44a3083c1528a5c26461cd2b9acd5908763c2b32
Loveneesh <l.ksingla2@gmail.com>
2022-09-29 18:13:54 +0530

    fixed

--- src/Services/Setup.js
+++ src/Services/Setup.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AXIOS = axios.create({
-  baseURL: "http://209.97.142.219:8080",
+  baseURL: "http://dev.zestgeek.com/invoiceGenrate/api",
 });
 
 AXIOS.interceptors.request.use(
